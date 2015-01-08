113d0c489a2e18707dc9d92b2c87ab3641f57489
Cade Guevara <cadeguevara@gmail.com>
2015-01-08 11:49:18 -0800

    lecture1. i read and made examples from lecture 1

--- practices.php
+++ practices.php
@@ -4,6 +4,83 @@
 	<title></title>
 </head>
 <body>
+<?php
+// example1
+class shoes {
+	//class body
+	//class body
+	//class body
+}
 
+class car {
+	//class body
+	//class body
+	//class body
+}
+
+class cellphones {
+	//class body
+	//class body
+	//class body
+}
+//example2
+$game1 = new game();
+$game2 = new game();
+
+$car1 = new car();
+$car2 = new car();
+
+$cellphones1 = new cellphones();
+$cellphones2 = new cellphones();
+//example3
+class shoe {
+	public $brand = nike;
+	public $series = air max 90;
+	public $model = black tiger camo;
+}
+
+class car {
+	public $make = bmw;
+	public $model = m3;
+	public $year = 1988;
+}
+
+class cellphones {
+	public $brand = samsung
+	public $type = galaxy s5
+	public $os = android
+}
+//example4
+$shoe1 = new shoe();
+print $shoe1->brand;
+brand;
+
+$car1 = new car();
+print $car1->make;
+make;
+
+$cellphones1 = new cellphones();
+print $cellphones1->brand;
+brand;
+//example5
+public function Method1( $argument, $another) {
+	// stuff
+	//stuff
+	//stuff
+}
+
+public function Method2( $argument, $another) {
+	// stuff
+	//stuff
+	//stuff
+}
+
+public function Method3( $argument, $another) {
+	// stuff
+	//stuff
+	//stuff
+}
+
+?>
 </body>
 </html>
